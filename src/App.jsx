10bca3a2fbc2d181ea4eed1e116e9a854095b81e
Paul Edwin Antonio Juan <l19350281@tuxtepec.tecnm.mx>
2023-02-27 14:51:40 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const name = "Paul AJ";
+  return <div>
+    <h3> Lista de Hobbies de {name} </h3>
+    <ul>
+        <li>Realizar dibujos</li>
+        <li>Aprender defensa personal</li>
+        <li>Leer</li>
+        <li>Videojuegos</li>
+        <li>Cocinar</li>
+        <li>Ver peliculas</li>
+    </ul>
+  </div>
+}
+
+export default App
